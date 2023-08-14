0d950840ded90dd78bbf9babf6cec72ce99e2eb5
Sheldon Hull <sheldonhull@users.noreply.github.com>
2023-08-14 15:38:41 -0500

    chore: improve output of trunk validation on renovate

--- .trunk/renovate-config-validator.sh
+++ .trunk/renovate-config-validator.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-#!/usr/bin/env bash
+# If npm global install package renovate is not found then install `npm install --global renovate`
+if ! command -v renovate-config-validator &>/dev/null; then
+    echo "renovate-config-validator could not be found, installing..."
+    npm install --global renovate-config-validator
+fi
 
 # Define an array of JSON files to validate
 json_files=("common.json" "default.json" "group-by-manager.json" "security.json")
@@ -9,8 +13,10 @@ for file in "${json_files[@]}"; do
     renovate-config-validator "$file"
     if [ $? -ne 0 ]; then
         # If the validator returns a non-zero exit code, exit with a 1
+        echo "❌ $file failed validation"
         exit 1
     fi
+    echo "✔️ $file passed validation"
 done
 
 # If all validators pass, exit with a 0
