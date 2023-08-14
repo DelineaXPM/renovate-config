8bfa5bd4e85a99279aa28af696f25bb6221e74a5
Sheldon Hull <sheldonhull@users.noreply.github.com>
2023-08-14 15:40:48 -0500

    chore: improve output of trunk validation on renovate

--- .trunk/renovate-config-validator.sh
+++ .trunk/renovate-config-validator.sh
@@ -11,12 +11,12 @@ json_files=("common.json" "default.json" "group-by-manager.json" "security.json"
 # Loop through each JSON file and run the config validator
 for file in "${json_files[@]}"; do
     renovate-config-validator "$file"
-    if [ $? -ne 0 ]; then
+    if [[ $? -ne 0 ]]; then
         # If the validator returns a non-zero exit code, exit with a 1
         echo "❌ $file failed validation"
         exit 1
     fi
-    echo "✔️ $file passed validation"
+    echo "✅ $file passed validation"
 done
 
 # If all validators pass, exit with a 0
